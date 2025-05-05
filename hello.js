ebf4d2f53b6552d97d29669d2415816c62628209
Sinthuha Nadesan <nadasinthu09@gmail.com>
2025-05-05 15:59:39 +0530

    javascript basic code

---
+++ hello.js
@@ -0,0 +1,14 @@
+var price = 100
+var product ="biscuit"
+var tax = 0.05*price
+var total = price+tax
+console.log(product)
+console.log(total)
+
+
+var fruitName = "apple"
+var count = 5
+var f_price = 100
+var f_total = count * f_price
+console.log(fruitName)
+console.log(f_total)

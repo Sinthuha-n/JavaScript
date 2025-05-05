92ff9651b7d1923abc33c67efdad3a4e6d18bbab
Sinthuha Nadesan <nadasinthu09@gmail.com>
2025-05-05 16:00:24 +0530

    basic function code

---
+++ func.js
@@ -0,0 +1,31 @@
+
+var a=10
+var b=20
+
+function add(){
+    console.log(a+b)
+}
+//add()
+
+factor = "Kamal"
+fplayer = "Dhoni"
+fmovie = "Anbe sivam"
+
+function favourite(){
+    console.log("Favourite Actor :"+factor)
+    console.log("Favourite Player :"+fplayer)
+    console.log("Favourite Movie :"+fmovie)
+}
+favourite()
+
+function add_num(x,y){
+    console.log(x+y)
+}
+//add_num(5,4)
+
+function ADD(p,q){
+    return(p+q)
+}
+var total =ADD(15,10)
+//console.log(total)
+

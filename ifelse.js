a244cf1fd90227ec805c19fc6e3f5e07269ac20c
Sinthuha Nadesan <nadasinthu09@gmail.com>
2025-05-06 11:27:32 +0530

    loop basic code

---
+++ ifelse.js
@@ -0,0 +1,43 @@
+//1
+var color = "red"
+if(color=="red"){
+    console.log("Stop")
+}
+if(color=="yellow"){
+    console.log("Get Ready")
+}
+if(color=="green"){
+    console.log("Go")
+}
+//2
+var season = "spring"
+if(season=="spring")
+{
+    console.log("blooming flowers")
+}
+if(season=="Summer"){
+    console.log("have fun in the sun")
+}
+if(season=="autumn")
+{
+    console.log("admire the colorful leaves")
+}
+if (season=="winter")
+{
+    console.log("bundle up and stay form")
+}
+
+//3
+var score = 75
+if(score<50)
+{
+    console.log("You need to improve")
+}
+if(score>50 && score<=70)
+{
+    console.log("Good job")
+}
+if(score>70)
+{
+    console.log("Excellent performance")
+}
